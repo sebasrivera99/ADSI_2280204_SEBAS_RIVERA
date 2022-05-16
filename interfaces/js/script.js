5a3af1da6676e8dede18bd22c51e30fe9bdb446f
sebasrivera99 <sebascruzmusic@gmail.com>
2022-05-16 11:38:05 -0500

    guardando img

--- interfaces/js/script.js
+++ interfaces/js/script.js
@@ -1,18 +1,28 @@
-function validarInfo(numeroDocumento,nombreUsuario,edad,direccion) {
-    let texto = document.getElementById(numeroDocumento);
-    let usuario = document.getElementById(nombreUsuario);
-    let eda = document.getElementById(edad);
-    let dir = document.getElementById(direccion);
-    if(texto.value === ""){
-        alert("Ingrese su documento");
+function guardarInfo() {
+    let numeroDocumento = document.getElementById("numeroDocumento").value;
+    if (numeroDocumento == "") {
+        alert("Ingresa el numero de documento");
+        return false;
     }
-    if (usuario.value === ""){
-        alert("Ingrese su nombre");
+    if (document.getElementById("nombreUsuario").value == "") {
+        alert("Ingrese el nombre del usuario");
+        return false;
     }
-    if (eda.value === ""){
+    if (document.getElementById("edad").value == "") {
         alert("Ingrese su edad");
+        return false;
     }
-    if (dir.value === ""){
+    if (document.getElementById("direccion").value == "") {
         alert("Ingrese su direccion");
+        return false;
     }
-  }
+    alert("Informacion guardada con exito");
+    limpiarformulario();
+    return true;
+}
+function limpiarformulario() {
+    document.getElementById("numeroDocumento").value = "";
+    document.getElementById("nombreUsuario").value = "";
+    document.getElementById("edad").value = "";
+    document.getElementById("direccion").value = "";
+}
