af1eb716c838522a76656bed463a591bb0388996
sebasrivera99 <sebascruzmusic@gmail.com>
2022-05-16 09:47:21 -0500

    formulando

--- interfaces/js/script.js
+++ interfaces/js/script.js
@@ -1,1 +1,18 @@
-alert ("HOLA MUNDO")
+function validarInfo(numeroDocumento,nombreUsuario,edad,direccion) {
+    let texto = document.getElementById(numeroDocumento);
+    let usuario = document.getElementById(nombreUsuario);
+    let eda = document.getElementById(edad);
+    let dir = document.getElementById(direccion);
+    if(texto.value === ""){
+        alert("Ingrese su documento");
+    }
+    if (usuario.value === ""){
+        alert("Ingrese su nombre");
+    }
+    if (eda.value === ""){
+        alert("Ingrese su edad");
+    }
+    if (dir.value === ""){
+        alert("Ingrese su direccion");
+    }
+  }
