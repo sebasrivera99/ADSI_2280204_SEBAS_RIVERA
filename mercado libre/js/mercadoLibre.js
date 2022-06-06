32fafc57dcab44f461c5a0ebb79357a8c22ea43c
sebasrivera99 <sebascruzmusic@gmail.com>
2022-06-06 10:08:12 -0500

    modificando

--- mercado libre/js/mercadoLibre.js
+++ mercado libre/js/mercadoLibre.js
@@ -14,9 +14,9 @@ class automovil{
 
 let car1= new automovil("Chevrolet","zl1","camaro","2021","335000000","38","Bogota","img/carro-camaro.jpg");
 let car2= new automovil("Ford","Mustang","GT350","2020","400000000","115","Cali","img/carro-ford.jpeg");
-let car3= new automovil("Nissan","300 ZX ","S111","2020","120000000","23","Medellin","img/nissan.jpg");
-
-function Busqueda(car){
+let car3= new automovil("Nissan","300 ZX ","S111","1990","120000000","1245","Medellin","img/nissan.jpg");
+let car4= new automovil("Nissan","sentra","B18","2018","200000000","2567","Medellin","img/nissan2.png");
+function mostrar(car){
     let contenedorPrincipal=document.getElementById("contenidoPrincipal");
     let contimg= document.createElement("div");
      contenedorPrincipal.appendChild(contimg);
@@ -25,7 +25,7 @@ function Busqueda(car){
     let img= document.createElement("img");
     img.setAttribute("src", car.imagen);
     img.setAttribute("class","img");
-    contenedorPrincipal.appendChild(img);
+    contimg.appendChild(img);
 
     let valorcar= document.createElement("label");
 
@@ -34,32 +34,32 @@ function Busqueda(car){
     let textValorCar= document.createTextNode("$" + precioFormat);
     valorcar.appendChild(textValorCar);
     valorcar.setAttribute("class","precio");
-    contenedorPrincipal.appendChild(valorcar);
+    contimg.appendChild(valorcar);
 
     let descripcion= document.createElement("label");
     let textdesccar=document.createTextNode(car.marca+" " +car.modelo+" "+ car.version);
     descripcion.appendChild(textdesccar);
     descripcion.setAttribute("class","descripcion");
 
-    contenedorPrincipal.appendChild(descripcion);
+    contimg.appendChild(descripcion);
 
     let yearcar=document.createElement("label");
         let textyear=document.createTextNode('Año ' + car.year);
     yearcar.appendChild(textyear);
     yearcar.setAttribute("class","year");
-    contenedorPrincipal.appendChild(yearcar);
+    contimg.appendChild(yearcar);
 
     let kilometrajecar=document.createElement("label");
     let textkilometraje=document.createTextNode(car.kilometraje+" kilometros");
     kilometrajecar.appendChild(textkilometraje);
     kilometrajecar.setAttribute("class","kilometraje");
-    contenedorPrincipal.appendChild(kilometrajecar);
+    contimg.appendChild(kilometrajecar);
 
     let city=document.createElement("label");
     let textcity=document.createTextNode('Ciudad '+ car.ciudad);
     city.appendChild(textcity);
     city.setAttribute("class","ciudad");
-    contenedorPrincipal.appendChild(city);
+    contimg.appendChild(city);
 
 }
 
@@ -69,14 +69,15 @@ window.addEventListener("keydown",function(event){
         console.log(Busq);
         cleanwindow();
         if(Busq=="chevrolet"){
-            Busqueda(car1);           
+            mostrar(car1);           
         }
         else if(Busq=="ford"){
-            Busqueda(car2);
+            mostrar(car2);
         }
         
         else if(Busq=="nissan"){
-            Busqueda(car3);
+            mostrar(car3);
+            mostrar(car4);
         }
         else{
             alert("sin coincidencias");
